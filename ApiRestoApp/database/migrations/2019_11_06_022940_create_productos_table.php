e687afb39fe01d3cae5a0160c3af5264ae4c9a74
Axel Mercado <comamercado@gmail.com>
2019-11-06 00:06:55 -0300

    Update

---
+++ ApiRestoApp/database/migrations/2019_11_06_022940_create_productos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->string('articulo');
+            $table->string('categoria');
+            $table->decimal('precio', 10 ,2);
+            $table->smallInteger('cantidad')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
