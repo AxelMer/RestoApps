4f87c08aac09a424ba53ac1350ffbb4ad8651cac
Ammercado <39321438+Ammercado@users.noreply.github.com>
2019-11-19 00:06:29 -0300

    19/11/2019

--- front-react/src/Modulos/Salon/index.js
+++ front-react/src/Modulos/Salon/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import Box from '@material-ui/core/Box';
 import axios from 'axios';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
 import Typography from '@material-ui/core/Typography';
 import DeckIcon from '@material-ui/icons/Deck';
-import Fab from '@material-ui/core/Fab';
 import '../../App.css';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import PeopleIcon from '@material-ui/icons/People';
+import Button from '@material-ui/core/Button';
+import Tooltip from '@material-ui/core/Tooltip';
+import Drawer from '@material-ui/core/Drawer';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 export default class Salon extends React.Component{
 constructor(props){
         super(props);
@@ -24,12 +26,42 @@ constructor(props){
         }
         //this.handleChangeCapacidad  = this.handleChangeCapacidad.bind(this);
         //this.handleChangeEstado  = this.handleChangeEstado.bind(this);
-        //this.openModal = this.openModal.bind(this);
-        //this.closeModal = this.closeModal.bind(this);
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
 }
 componentDidMount(){
         this.loadData()
 }
+openModal() {
+    this.setState({ open: true });
+            return(
+                <Dialog open={this.state.open} onClose={this.closeModal}  disableBackdropClick aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description"> 
+                  <DialogContent>
+                    <DialogTitle id="alert-dialog-title">Modificar datos</DialogTitle>
+                      <DialogContentText id="alert-dialog-description">
+                          Actualizar datos.
+                      </DialogContentText>
+                      <DialogContent>
+             
+                      </DialogContent>
+                      <DialogActions>
+                            <Button  color="primary">
+                              Cancel
+                            </Button>
+                            <Button  color="primary">
+                              Actualizar
+                            </Button>
+                          </DialogActions>
+                  </DialogContent>
+                </Dialog>
+            )
+   
+}
+closeModal() {
+    this.setState({ open: false });
+}
+
+
 loadData = (e) =>{
         axios.get('http://localhost:8000/Mesa')
         .then(response=>{
@@ -42,28 +74,31 @@ loadData = (e) =>{
   renderList(){
     return this.state.lista.map((data)=>{
         return(
-            <div className="Contenido">
-                            <Grid item xs={10}>
-                                <Paper>
-                                    <i>{data.id}</i>
-                                    <br/>
-                                    {data.estado === 1 ? 
-                    <DeckIcon className="r" fontSize="large" /> 
+            <Grid item xs={2} zeroMinWidth>
+                <Paper>
+                    {data.estado === 1 ? 
+                        <Tooltip title="Add" placement="top">
+                            <Button onClick={this.openModal}>
+                                <DeckIcon className="r" fontSize="large" /> 
+                            </Button>
+                        </Tooltip>      
                     :
-                    <DeckIcon className="v" fontSize="large" />
-                }<hr/><PeopleIcon fontSize="small" /><i >{data.capacidad}</i>
+                    <Tooltip title={data.id} placement="top">
+                    <Button><DeckIcon className="v" fontSize="large" /> </Button>
+                </Tooltip>  
+                }<hr/>  
+                    <i><PeopleIcon fontSize="small"/>
+                        <Typography>{data.capacidad}</Typography></i>
                                     <hr/>
                             </Paper>
-                            </Grid>
-            </div>
-               
+            </Grid>   
         )
       })
       }
 
     render(){
         return(
-            <Grid container spacing={1}>
+            <Grid className='centrar'>
                 <Grid container item xs={20} spacing={1}>
                     {this.renderList()}
                 </Grid>
